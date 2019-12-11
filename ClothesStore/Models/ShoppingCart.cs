3c4ff57dfafc987c7d5e1e8e4ccd7ffc7727c28f
Karmo <Karmo.Kiima@Eesti.ee>
2019-12-11 10:53:06 +0200

    Add ShoppingCart model

---
+++ ClothesStore/Models/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClothesStore.Models
+{
+    public class ShoppingCart
+    {
+        private Guid ShoppingcartId { get; set; }
+        private DateTime DateCreated { get; set; }
+        private bool isPaid { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+
+        public ShoppingCart()
+        {
+            ShoppingcartId = Guid.NewGuid();
+            DateCreated = DateTime.Now;
+
+            Items = new List<Item>();
+        }
+    }
+}
