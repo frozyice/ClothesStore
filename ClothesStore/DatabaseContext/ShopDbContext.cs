152fe45d5b8e683215adb756e24c96668c969c21
Karmo <Karmo.Kiima@Eesti.ee>
2019-12-11 11:21:42 +0200

    Add DatabaseContext

---
+++ ClothesStore/DatabaseContext/ShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothesStore.DatabaseContext
+{
+    public class ShopDbContext
+    {
+        public class ShopDbContext : DbContext
+        {
+            public DbSet<Item> Items { get; set; }
+            public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            }
+        }
+    }
+}
---
+++ ClothesStore/DatabaseContext/ShopDbinitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothesStore.DatabaseContext
+{
+    public class ShopDbinitializer
+    {
+        public class ShopDbinitializer : DropCreateDatabaseIfModelChanges<ShopDbContext>
+        {
+            protected override void Seed(ShopDbContext context)
+            {
+                context.Items.AddOrUpdate(x => x.Name,
+                new Item
+                {
+                    Name = "Jeans",
+                    
+                };
+
+                
+            }
+        }
+    }
+}
