c218528081e8b062dc3b3f7894537f607ebb74de
Karmo <Karmo.Kiima@Eesti.ee>
2019-12-11 10:23:25 +0200

    Add Item model

---
+++ ClothesStore/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothesStore.Models
+{
+    public class Item
+    {
+        private string Name { get; set; }
+        private enum Category
+        {
+            Men = 0,
+            Women = 1,
+            Kids = 2
+        }
+        private List<string> AvailableSizes { get; set; }
+        private double Price { get; set; }
+
+        public Item()
+        {
+            AvailableSizes = new List<string>
+            {
+                "XS", "S", "M", "L", "XL", "XXL"
+            };
+        }
+    }
+}
